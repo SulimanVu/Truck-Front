02de6598145fc74712adbee90d971ebef0a607a4
Suliman <sssuliman@bk.ru>
2022-09-15 11:58:39 +0300

    Скачал toastify

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import styles from "./header.module.scss";
 import logo from "../../public/logo_2.png";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const token = useSelector((state) => state.application.token);
+
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
@@ -16,17 +19,24 @@ const Header = () => {
           <Link to="/cars" className={styles.link}>
             <li>cars</li>
           </Link>
-          <Link to="/user/:id" className={styles.link}>
-            <li>profile</li>
-          </Link>
-          <Link to="/cars" className={styles.link}>
-            <li>Service</li>
+          {token ? (
+            <Link to="/user/:id" className={styles.link}>
+              <li>profile</li>
+            </Link>
+          ) : (
+            <li className={styles.link_off}>profile</li>
+          )}
+          <Link to="/" className={styles.link}>
+            <li>main</li>
           </Link>
           <Link to="/about" className={styles.link}>
             <li>about us</li>
           </Link>
-          <Link to="/" className={styles.link}>
-            <li>Main</li>
+          <Link to="/signin" className={styles.link}>
+            <li className={styles.link_mg}>sign in</li>
+          </Link>
+          <Link to="/signup" className={styles.link}>
+            <li>sign up</li>
           </Link>
         </ul>
       </div>
