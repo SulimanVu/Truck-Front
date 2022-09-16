039656c3dfa8e9e5cbff21101cd3d1728bbf06e8
Suliman <sssuliman@bk.ru>
2022-09-16 11:11:54 +0300

    Подправил About

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import Header from "../../components/Header/Header";
-import HeaderMain from "../../components/HeaderMain/HeaderMain";
 import styles from "./about.module.scss";
 
 const About = () => {
   return (
     <>
-      <HeaderMain />
+      <Header />
       <section className={styles.general}>
-        <div className={styles.info}>
-          <h1>Гарантия.</h1>
-          <h4>
-            Пользуясь нашими услугами, Вы тем самым автоматически страхуете свой
-            груз и не переживаете за его сохранность.
-          </h4>
+        <div className={styles.left_block}></div>
+        <div className={styles.right_block}>
+          <div className={styles.info}>
+            <h1>Гарантия.</h1>
+            <h4>
+              Пользуясь нашими услугами, Вы тем самым автоматически страхуете
+              свой груз и не переживаете за его сохранность.
+            </h4>
+          </div>
           <div className={styles.info2}>
             <h1>Отвественность.</h1>
             <h4>
