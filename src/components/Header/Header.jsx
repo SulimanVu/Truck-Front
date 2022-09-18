2831ef109c97ab4f3c7a83145ed063eb112b44a7
Waldgang <waldgang@MacBook-Air-salambek.local>
2022-09-19 01:23:56 +0300

    Добавил немного стилей

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./header.module.scss";
-import logo from "../../public/logo.png";
+import logo from "../../public/logo_2.png";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { motion } from "framer-motion";
@@ -13,8 +13,8 @@ const Header = () => {
     <header className={styles.header}>
       <motion.div
         transition={{ duration: 2 }}
-        initial={{ x: -100, rotate: -45, y: -5 }}
-        animate={{ x: 0, rotate: [-45, -45, -45, -45, 0], y: 0 }}
+        initial={{ x: -109, rotate: 0, y: -40 }}
+        animate={{ x: 0, rotate: 0, y: -6}}
         className={styles.logo}
       >
         <Link to="/">
--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -4,11 +4,17 @@ import Header from "../../components/Header/Header";
 import User from "../../components/User/User";
 import styles from "./userpage.module.scss";
 import { Link, Navigate } from "react-router-dom";
-import RequestForm from "../RequestForm/RequestForm";
+import RequestForm from "../requestForm/RequestForm";
 
 const UserPage = () => {
   const [path, setPath] = useState("user");
-  
+  const [alert, setAlert] = useState(false)
+
+  const handleRequest = () => {
+    setPath("request")
+    setAlert(false)
+  }
+
   return (
     <>
       <Header />
@@ -16,13 +22,14 @@ const UserPage = () => {
         <div className={styles.request}>
           {path === "user" && <User />}
           {path === "request" && <Request />}
-          {path === "addRequest" && <RequestForm />}
+          {path === "addRequest" && <RequestForm setAlert={setAlert}/>}
           {path === "card" && <Navigate to={"/cars"} />}
         </div>
         <div className={styles.aside}>
           <div onClick={() => setPath("user")}>Личная страница</div>
-          <div onClick={() => setPath("request")}>Заявки</div>
+          <div onClick={() => handleRequest()}>Заявки</div>
           <div onClick={() => setPath("addRequest")}>Сделать заявку</div>
+          <div className={alert ? styles.alert : styles.alertoff}><p>1</p></div>
           <Link to="/" className={styles.Link}>
             <div>Выйти из аккаунта</div>
           </Link>
